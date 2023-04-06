aff94440c999d1d74b4d73b17190677e3a78b3dd
Abhiraj Kale <abhirajkale1806@gmail.com>
2023-04-06 12:05:50 +0530

    10 files getting created with ejs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,64 @@
-console.log('Hello world');
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// eslint-disable-next-line node/no-unpublished-import
+import axios from 'axios';
+import fs = require('fs');
+import path = require('path');
+import ejs = require('ejs');
+
+interface Data {
+  activity: string;
+  type: string;
+  participants: number;
+  price: number;
+  link: string;
+  key: string;
+  accessibility: number;
+}
+
+async function getUsers() {
+  console.log('10 dynamically generated pages: ');
+
+  for (let i = 0; i < 10; i++) {
+    try {
+      const options = {
+        method: 'GET',
+        url: 'https://www.boredapi.com/api/activity',
+      };
+
+      axios
+        .request(options)
+        .then(({data}: {data: Data}) => {
+          console.log(data);
+          console.log(data.accessibility);
+
+          ejs.renderFile(
+            path.join(__dirname, 'views') + '/index.ejs',
+            {data: data},
+            (err: any, html: any) => {
+              if (err) {
+                throw err;
+              }
+
+              fs.writeFile(
+                path.join(__dirname, 'views') + '/file' + (i + 1) + '.html',
+                html,
+                (err: any) => {
+                  if (err) {
+                    throw err;
+                  }
+                  console.log('File saved!');
+                }
+              );
+            }
+          );
+        })
+        .catch((error: any) => {
+          console.error(error);
+        });
+    } catch (error: any) {
+      console.error(error);
+    }
+  }
+}
+
+getUsers();
